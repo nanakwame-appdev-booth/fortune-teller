7b613c950e9b0b171048d65e297342ca553d1404
jasonantwi <jantwi@chicagobooth.edu>
2021-07-21 04:23:12 +0000

    Fixed Capricorn (Stuck on Sag)

--- config/routes.rb
+++ config/routes.rb
@@ -17,11 +17,11 @@ Rails.application.routes.draw do
 
   get("/zodiacs/aries", { :controller =>  "fires", :action =>  "ram" })
   get("/zodiacs/leo", { :controller => "fires", :action => "lion" })
-  get("/zodiacs/sagittarius", { :controller => "fire", :action => "archer" })
+  # get("/zodiacs/sagittarius", { :controller => "fire", :action => "archer" })
   
   get("/zodiacs/taurus", { :controller => "earth", :action => "bull" })
   get("/zodiacs/virgo", { :controller => "earth", :action => "maiden" })
-  # get("/zodiacs/capricorn", { :controller => "earth", :action => "goat" })
+  get("/zodiacs/capricorn", { :controller => "earth", :action => "goat" })
   
   get("/zodiacs/gemini", { :controller => "air", :action => "twins" })
   get("/zodiacs/libra", { :controller => "air", :action => "scales" })
